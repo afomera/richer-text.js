00f0ba2be02e4ffcef1c69e03bf7733ab39dcd1c
Andrea Fomera <afomera@hey.com>
2022-12-30 21:53:00 -0700

    basic helloworld component to validate things work

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("Hello from RicherText.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.HelloWorld = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+console.log("RicherText - Loaded");
+var HelloWorld = function HelloWorld(_ref) {
+  var name = _ref.name;
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello ", name, "!"));
+};
+exports.HelloWorld = HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from RicherText.js");
+console.log("RicherText - Loaded");
+
+import React from "react";
+
+export const HelloWorld = ({name}) => {
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+    </div>
+  );
+}
