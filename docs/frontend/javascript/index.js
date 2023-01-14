94e0d3a1db02455682a888aa87316ad4a8bf43f1
Andrea Fomera <afomera@hey.com>
2023-01-14 15:07:06 -0700

    reorder JS

--- docs/frontend/javascript/index.js
+++ docs/frontend/javascript/index.js
@@ -1,10 +1,8 @@
-
-import "@afomera/richer-text"
-
 import "index.css"
 import "syntax-highlighting.css"
-import "@afomera/richer-text/dist/css/richer-text.css"
 import "highlight.js/styles/github-dark.css"
+import "@afomera/richer-text"
+import "@afomera/richer-text/dist/css/richer-text.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
