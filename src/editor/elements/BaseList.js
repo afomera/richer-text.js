2a798eacbefbbbfd513deb3edc5ffeca8199e29a
Andrea Fomera <afomera@hey.com>
2024-03-23 22:12:02 -0600

    Add BaseList and extract the render and selectItem functions out to subclasses

---
+++ src/editor/elements/BaseList.js
@@ -0,0 +1,90 @@
+import { html, css, LitElement } from 'lit';
+
+export class BaseList extends LitElement {
+  static styles = css`
+    .suggested-items {
+      padding: 0.2rem;
+      position: relative;
+      border-radius: 0.5rem;
+      background: #fff;
+      color: rgba(0, 0, 0, 0.8);
+      overflow: hidden;
+      font-size: 0.9rem;
+      box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05), 0px 10px 20px rgba(0, 0, 0, 0.1);
+    }
+    .suggested-item {
+      display: flex;
+      align-items: center;
+      margin: 0;
+      width: 100%;
+      text-align: left;
+      background: transparent;
+      border-radius: 0.4rem;
+      border: 1px solid transparent;
+      padding: 0.2rem 0.4rem;
+
+      img {
+        border-radius: 50%;
+        margin-right: 0.4rem;
+      }
+    }
+    .suggested-item.is-selected {
+      border-color: #000;
+    }
+  `
+
+  static properties = {
+    items: { type: Array },
+    selectedIndex: { type: Number },
+    command: { type: Function }
+  }
+
+  constructor() {
+    super();
+    this.items = [];
+    this.selectedIndex = 0;
+    this.command = () => {};
+  }
+
+  render() {
+    return html``;
+  }
+
+  onKeyDown({event}) {
+    if (event.key === 'ArrowUp') {
+      this.upHandler()
+      return true
+    }
+
+    if (event.key === 'ArrowDown') {
+      this.downHandler()
+      return true
+    }
+
+    if (event.key === 'Enter') {
+      this.enterHandler()
+      return true
+    }
+
+    return false
+  }
+
+  upHandler() {
+    this.selectedIndex = ((this.selectedIndex + this.items.length) - 1) % this.items.length
+  }
+
+  downHandler() {
+    this.selectedIndex = (this.selectedIndex + 1) % this.items.length
+  }
+
+  enterHandler() {
+    this.selectItem(this.selectedIndex)
+  }
+
+  selectItem(index) {
+    // This method should be implemented in the extending class
+  }
+}
+
+// Don't register this element directly as we don't want to use it in the editor and want to extend it in the MentionList element
+// customElements.define('richer-text-base-list', BaseList);
--- src/editor/elements/MentionList.js
+++ src/editor/elements/MentionList.js
@@ -1,51 +1,7 @@
-import { html, css, LitElement } from 'lit';
+import { html } from 'lit';
+import { BaseList } from './BaseList';
 
-export class MentionList extends LitElement {
-  static styles = css`
-    .suggested-items {
-      padding: 0.2rem;
-      position: relative;
-      border-radius: 0.5rem;
-      background: #fff;
-      color: rgba(0, 0, 0, 0.8);
-      overflow: hidden;
-      font-size: 0.9rem;
-      box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05), 0px 10px 20px rgba(0, 0, 0, 0.1);
-    }
-    .suggested-item {
-      display: flex;
-      align-items: center;
-      margin: 0;
-      width: 100%;
-      text-align: left;
-      background: transparent;
-      border-radius: 0.4rem;
-      border: 1px solid transparent;
-      padding: 0.2rem 0.4rem;
-
-      img {
-        border-radius: 50%;
-        margin-right: 0.4rem;
-      }
-    }
-    .suggested-item.is-selected {
-      border-color: #000;
-    }
-  `
-
-  static properties = {
-    items: { type: Array },
-    selectedIndex: { type: Number },
-    command: { type: Function }
-  }
-
-  constructor() {
-    super();
-    this.items = [];
-    this.selectedIndex = 0;
-    this.command = () => {};
-  }
-
+export class MentionList extends BaseList {
   render() {
     return html`
       <div class="suggested-items">
@@ -61,37 +17,6 @@ export class MentionList extends LitElement {
           ) : html`<div class="suggested-item">No results</div>`}
       </div>
     `;
-  }
-
-    onKeyDown({event}) {
-    if (event.key === 'ArrowUp') {
-      this.upHandler()
-      return true
-    }
-
-    if (event.key === 'ArrowDown') {
-      this.downHandler()
-      return true
-    }
-
-    if (event.key === 'Enter') {
-      this.enterHandler()
-      return true
-    }
-
-    return false
-  }
-
-  upHandler() {
-    this.selectedIndex = ((this.selectedIndex + this.items.length) - 1) % this.items.length
-  }
-
-  downHandler() {
-    this.selectedIndex = (this.selectedIndex + 1) % this.items.length
-  }
-
-  enterHandler() {
-    this.selectItem(this.selectedIndex)
   }
 
   selectItem(index) {
